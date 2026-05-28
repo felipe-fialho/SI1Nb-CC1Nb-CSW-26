85b9f12ef60d41389c2745eb8aa9a7a3481a2b66
Felipe Fialho Ferreira <s.fransciscoxavier@gmail.com>
2026-05-27 22:27:55 -0300

    Add files via upload

---
+++ aula_16/javascript.js
@@ -0,0 +1,9 @@
+let dia = document.getElementById("dia").value;
+let mes = document.getElementById("mes").value;
+let ano = document.getElementById("ano").value;
+
+const hoje = new Date();
+
+if()
+
+document.getElementById("resultado").textContent = "Você tem" 
